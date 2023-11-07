820dcfea7c7a1a127cd1a92e369b93e2dc466c6c
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2023-11-07 11:48:35 +0200

    update tasks

---
+++ js/task-1.js
@@ -0,0 +1,20 @@
+`use strict`;
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Stone skin", price: 520 },
+  ],
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    this.potions.push(newPotion);
+  },
+  getTotalPrice() {
+    let totalPrice = 0;
+    for (const potion of potions) {
+      totalPrice += potion.price;
+    }
+    return totalPrice;
+  },
+};
---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+const atTheOldToard = {
+  potions: [],
+  getPotions() {
+    return "List of all available potions";
+  },
+  addPotion(potionName) {
+    return `Adding ${potionName}`;
+  },
+};
