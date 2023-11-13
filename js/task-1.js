c2e117c618750f0f79715b872347c5b7708d4bb4
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2023-11-13 20:20:45 +0200

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,11 @@
 `use strict`;
-const atTheOldToad = {
-  potions: [
+
+function isEnoughCapacity(products, containerSize) {
+
+  const potions = {
     { name: "Speed potion", price: 460 },
-    { name: "Stone skin", price: 520 },
-  ],
+  { name: "Stone skin", price: 520 },
+},
   getPotions() {
     return this.potions;
   },
@@ -18,3 +20,20 @@ const atTheOldToad = {
     return totalPrice;
   },
 };
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
+
